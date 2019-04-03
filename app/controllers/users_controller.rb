b665359e76874dd2254201d3390466583346dc94
Omid Afshar <omidafshar@berkeley.edu>
2019-04-02 23:04:26 -0700

    Saving user preferences to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
+	#def user_params
+	#	params.permit(:age, :weight, :height, :exercise, :goal, :budget, :time, :cuisine, :gender)
+	#end
+
 	def show
-		@age = :age
+		@age = current_user.age
 	    @gender = :gender
 	    @weight = :weight
 	    @height = :height
@@ -10,10 +14,20 @@ class UsersController < ApplicationController
 	    @budget = :budget
 	    @time = :time
 	    @cuisine = :cuisine
+	    @calories = 1 
 	end
 
 	def update
 		#save form data for user
+		current_user.age = params[:age]["age"].to_i
+		current_user.weight = params[:weight]["weight"].to_i
+		current_user.height = params[:height]["height"].to_i
+		current_user.budget = params[:budget]["budget"].to_i
+		current_user.time = params[:time]["time"].to_i
+		current_user.gender = params[:gender]
+		current_user.exercise = params[:exercise]
+		current_user.cuisine = params[:cuisine]["cuisine"]
+		current_user.save!
 		redirect_to show_path
 	end
 end
