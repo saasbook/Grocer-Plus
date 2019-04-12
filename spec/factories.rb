cd61bd7dab30b56f0420eab7a1028849ad3531a3
Omid Zargham <ozargham@berkeley.edu>
2019-04-12 14:33:05 -0700

    Add WebMock and FactoryGirl gems and set up testing infrastructure for rspec

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+    factory :user do
+        email "aabb@hh.de"
+        password "ruby123"
+        height 1
+        weight 1
+        exercise "Light"
+        age 12
+
+    end
+end
