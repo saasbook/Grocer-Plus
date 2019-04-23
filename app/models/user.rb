fcd999c817db43540bcefd8cbb3121f9ee938e1b
ChloeGuyue <muxuan_lyu@berkeley.edu>
2019-04-23 16:56:42 -0700

    added recipe model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_and_belongs_to_many :recipes
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         
 end
