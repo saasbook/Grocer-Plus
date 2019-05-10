ec0f57fef50112cd93cdc5bed20bb0798e9d73aa
Omid Afshar <omidafshar@berkeley.edu>
2019-05-10 02:39:11 -0700

    Added image and link to factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
         calories { 200 }
         time { 45 }
         meal_type { "breakfast" }
+        image { "https://www.edamam.com/web-img/59e/59e3c358ff7f844909e8e5dae7a3299c.jpg" }
+        link {  "http://www.edamam.com/recipe/tomato-and-bacon-breakfast-sandwiches-recipe-601fe3b9178e4df9a86d1aa7abf34550/breakfast/515-715-cal" }
         user
     end
 
