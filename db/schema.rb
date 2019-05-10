71b1efc3fa258dd19e7c56057593f99819750279
Omid Zargham <ozargham@berkeley.edu>
2019-05-10 13:51:33 -0700

    apply migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190510011333) do
+ActiveRecord::Schema.define(version: 20190510072618) do
 
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
