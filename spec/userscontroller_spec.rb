b95d0de5e4175b4d0e50085e9ceb98f01992bad3
CJ Jang <cjang000@berkeley.edu>
2019-05-12 00:12:06 -0700

    Adjusted tests

--- spec/userscontroller_spec.rb
+++ spec/userscontroller_spec.rb
@@ -130,39 +130,39 @@ describe UsersController, :type => :controller do
 
     describe "Calculate Calories" do
       it "Should calculate calories for men" do
-        UsersController.calc_calories('Male', 80, 180, 25, 'Light', 'Maintain') == 2481.875
+        UsersController.calc_calories(['Male', 80, 180, 25, 'Light', 'Maintain']) == 2481.875
       end
 
       it "Should calculate calories for varying weight" do
-        UsersController.calc_calories('Male', 90, 180, 25, 'Light', 'Maintain') == 2619.375
+        UsersController.calc_calories(['Male', 90, 180, 25, 'Light', 'Maintain']) == 2619.375
       end
 
       it "Should calculate calories for varying height" do
-        UsersController.calc_calories('Male', 80, 170, 25, 'Light', 'Maintain') == 2395.9375
+        UsersController.calc_calories(['Male', 80, 170, 25, 'Light', 'Maintain']) == 2395.9375
       end
 
       it "Should calculate calories for varying ages" do
-        UsersController.calc_calories('Male', 80, 180, 40, 'Light', 'Maintain') == 2378.75
+        UsersController.calc_calories(['Male', 80, 180, 40, 'Light', 'Maintain']) == 2378.75
       end
 
       it "Should calculate calories for moderate exercise level" do
-        UsersController.calc_calories('Male', 80, 180, 25, 'Moderate', 'Maintain') == 2797.75
+        UsersController.calc_calories(['Male', 80, 180, 25, 'Moderate', 'Maintain']) == 2797.75
       end
 
       it "Should calculate calories for heavy exercise level" do
-        UsersController.calc_calories('Male', 80, 180, 25, 'Heavy', 'Maintain') == 3113.624
+        UsersController.calc_calories(['Male', 80, 180, 25, 'Heavy', 'Maintain']) == 3113.624
       end
 
       it "Should calculate calories for losing weight" do
-        UsersController.calc_calories('Male', 80, 180, 25, 'Light', 'Lose') == 1981.875
+        UsersController.calc_calories(['Male', 80, 180, 25, 'Light', 'Lose']) == 1981.875
       end
 
       it "Should calculate calories for gaining weight" do
-        UsersController.calc_calories('Male', 80, 180, 25, 'Light', 'Gain') == 2981.875
+        UsersController.calc_calories(['Male', 80, 180, 25, 'Light', 'Gain']) == 2981.875
       end
 
       it "Should calculate calories for women" do
-        UsersController.calc_calories('Female', 80, 180, 25, 'Light', 'Maintain') == 2315.875
+        UsersController.calc_calories(['Female', 80, 180, 25, 'Light', 'Maintain']) == 2315.875
       end
     end
     describe "Favorite a Recipe" do
