2ce298215518dacdb2faeb4ceed6eec7db587a55
Neha Kompella <nkompella@berkeley.edu>
2019-05-14 22:50:20 -0700

    fixed home routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,6 @@ class PagesController < ApplicationController
       redirect_to edit_path
     elsif current_user and current_user.recipes 
       redirect_to show_path
-    else
-      redirect_to new_user_session_path
     end
   end
 end
